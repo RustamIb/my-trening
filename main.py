55917fa2e81eac879b4fcd412d580f816c24072f
RustamIb <rustamkharr@gmail.com>
2025-01-09 18:19:23 +0500

    09/01/2025 Практическое задание по уроку "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,11 @@
+#Практическое задание по уроку "Базовые структуры данных"
+print("12+89-1")
+# 1 st programm
+print(9**0.5*5)
+# 2 nd programm
+print(9.99>9.98 and 1000!=1000.1)
+# 3 rd programm
+print(2*2+2==2*(2+2))
+# 4 th programm
+print(int(123.456*10)-1230)
+print(int(123.456*10)%10)
